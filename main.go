01098a37b98d45a440ab8543dc257e833d0217db
XieWei <xie_wei_sh@foxmail.com>
2023-05-10 18:45:00 +0800

    feat: 接入飞书

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	sdkginext "github.com/larksuite/oapi-sdk-gin"
+	lark "github.com/larksuite/oapi-sdk-go/v3"
+	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
+	"github.com/larksuite/oapi-sdk-go/v3/event/dispatcher"
+	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
 	"net/http"
 	"time"
 
@@ -12,6 +18,30 @@ import (
 func main() {
 	s := gin.New()
 	s.Use(gin.Recovery(), gin.Logger(), gin.ErrorLogger())
+
+	client := lark.NewClient("", "")
+
+	handler := dispatcher.NewEventDispatcher("verificationToken", "eventEncryptKey").
+		OnP2MessageReceiveV1(func(ctx context.Context, event *larkim.P2MessageReceiveV1) error {
+			fmt.Println(larkcore.Prettify(event))
+			fmt.Println(event.RequestId())
+			tenantKey := event.TenantKey()
+			resp, err := client.Im.Message.Create(context.Background(), larkim.NewCreateMessageReqBuilder().
+				ReceiveIdType(larkim.ReceiveIdTypeOpenId).
+				Body(larkim.NewCreateMessageReqBodyBuilder().
+					MsgType(larkim.MsgTypePost).
+					ReceiveId("ou_c245b0a7dff2725cfa2fb104f8b48b9d").
+					Content("text").
+					Build()).
+				Build(), larkcore.WithTenantKey(tenantKey))
+			fmt.Println(resp, err)
+			return nil
+		}).
+		OnP2MessageReadV1(func(ctx context.Context, event *larkim.P2MessageReadV1) error {
+			fmt.Println(larkcore.Prettify(event))
+			fmt.Println(event.RequestId())
+			return nil
+		})
 
 	v1 := s.Group("http-bin/v1")
 	controller := api.HttpBinUS{}
@@ -42,6 +72,9 @@ func main() {
 
 	userAgentGroup := v1.Group("userAgents")
 	userAgentGroup.GET("userAgent", controller.UserAgent)
+
+	webhook := v1.Group("feishu")
+	webhook.POST("webhook/event", sdkginext.NewEventHandlerFunc(handler))
 
 	_ = s.Run(":9091")
 
